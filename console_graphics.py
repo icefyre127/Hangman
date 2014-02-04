bd647d1a578799e6fe5fdc20c1bb166043c048bd
Will <gogobebe2@gmail.com>
2014-02-04 17:58:16 +1300

    Create console_graphics.py

---
+++ console_graphics.py
@@ -0,0 +1,7 @@
+import time
+
+#If this file is executed, then display the below error message and close the program
+if __name__ == '__main__':
+    print("[ERROR]: Do not run this file. Run 'Run.py' - this file should not be executed!")
+    time.sleep(4)
+    exit()
