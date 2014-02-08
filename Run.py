3c927bba4c8320f5b2379d3c34a2a4571fb63b1b
Michael Harris <mharris29@outlook.com>
2014-02-08 02:09:31 +0000

    Added a graphics file and a title file and some testing code for reading the files. Next step is to build the game logic.

--- Run.py
+++ Run.py
@@ -1,6 +1,36 @@
+#!/usr/bin/python
 #if the file is the main file then start the program
-if __name__ == '__main__':
-    init()
+DICTIONARY = "words.txt"
+TITLE_FILE="title.txt"
+GRAPHIC_FILE = "hanged man.txt"
+INDENT = ""
+
+class Tile:
+	def __init__(self,ch = None):
+		self.character = ch
+		self.visible = false
+
+		
+
+screenMatrix = [[" " for x in xrange(5)] for x in xrange(5)] 
+
+
+def printTitle():
+   with open(TITLE_FILE) as titleFile:
+      title = titleFile.read()
+      print title
+
+def showHangman():
+   with open(GRAPHIC_FILE) as titleFile:
+      title = titleFile.read()
+      print title
+
+def newLine(num = 1):
+    for i in range(num): print
     
+if __name__ == '__main__':
+    printTitle()
+    newLine(5)
+    showHangman()
 def init():
     pass
